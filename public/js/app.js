f574eb7e439ed16aea5162e3b5645b88b8bdae4b
Renato Porto <renatoafporto@gmail.com>
2020-03-28 20:56:31 -0300

    Changed forecast message format

--- public/js/app.js
+++ public/js/app.js
@@ -2,9 +2,6 @@ const weatherForm = document.querySelector("form");
 const search = document.querySelector("input");
 const messageOne = document.querySelector("#message-1");
 const messageTwo = document.querySelector("#message-2");
-const messageThree = document.querySelector("#message-3");
-const messageFour = document.querySelector("#message-4");
-const messageFive = document.querySelector("#message-5");
 
 weatherForm.addEventListener("submit", e => {
   e.preventDefault();
@@ -12,39 +9,24 @@ weatherForm.addEventListener("submit", e => {
   const location = search.value;
   messageOne.textContent = "Loading...";
   messageTwo.textContent = "";
-  messageThree.textContent = "";
-  messageFour.textContent = "";
-  messageFive.textContent = "";
 
   fetch("/weather?address=" + location).then(response => {
     response.json().then(({ error, location, forecast }) => {
       if (error) {
         messageOne.textContent = error;
       } else {
-        const {
-          temperature,
-          temperatureHigh,
-          temperatureLow,
-          precipProbability,
-          summaryCurrently,
-          summaryDaily
-        } = forecast;
         messageOne.textContent = location;
         messageTwo.textContent =
-          "Temperature now: " +
-          temperature +
-          " °F / Weather: " +
-          summaryCurrently +
-          ".";
-        messageThree.textContent =
-          "Temperature (MAX/MIN): " +
-          temperatureHigh +
-          "/" +
-          temperatureLow +
-          " °F";
-        messageFour.textContent =
-          "Chance of precipitation: " + precipProbability + " %";
-        messageFive.textContent = "Day forecast: " + summaryDaily;
+          forecast.summaryDaily +
+          " It is currently " +
+          forecast.temperature +
+          " degrees out. High for today is " +
+          forecast.temperatureHigh +
+          " with a low of " +
+          forecast.temperatureLow +
+          ". There is a " +
+          forecast.precipProbability +
+          "% change of rain.";
       }
     });
   });
